f4d51e271631cec71a284e61d2205c7d6ff20c8e
LongPhamHai <dotrucbk@gmail.com>
2026-03-21 16:43:32 +0700

    a

--- main.c
+++ main.c
@@ -25,6 +25,20 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
     printf("Message arrived\n");
     printf("     topic: %s\n", topicName);
     printf("   message: %.*s\n", message->payloadlen, (char*)message->payload);
+    char *json_str = malloc(message->payloadlen + 1);
+    memcpy(json_str, message->payload, message->payloadlen);
+    json_str[message->payloadlen] = '\0';
+    cJSON *root = cJSON_Parse(json_str);
+    if (root == NULL) {
+        printf("JSON parse error\n");
+    } else {
+        cJSON *msg = cJSON_GetObjectItem(root, "msg");
+        if (cJSON_IsString(msg) && msg->valuestring != NULL) {
+            printf("msg: %s\n", msg->valuestring);
+        }
+        cJSON_Delete(root);
+    }
+    free(json_str);
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
     return 1;
@@ -38,77 +52,77 @@ void connlost(void *context, char *cause)
  
 int main(int argc, char* argv[])
 {
-//     MQTTClient client;
-//     MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-//     int rc;
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    int rc;
  
-//     if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID,
-//         MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
-//     {
-//         printf("Failed to create client, return code %d\n", rc);
-//         rc = EXIT_FAILURE;
-//         goto exit;
-//     }
- 
-//     if ((rc = MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered)) != MQTTCLIENT_SUCCESS)
-//     {
-//         printf("Failed to set callbacks, return code %d\n", rc);
-//         rc = EXIT_FAILURE;
-//         goto destroy_exit;
-//     }
- 
-//     conn_opts.keepAliveInterval = 20;
-//     conn_opts.cleansession = 1;
-//     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
-//     {
-//         printf("Failed to connect, return code %d\n", rc);
-//         rc = EXIT_FAILURE;
-//         goto destroy_exit;
-//     }
- 
-//     printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
-//            "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
-//     if ((rc = MQTTClient_subscribe(client, TOPIC, QOS)) != MQTTCLIENT_SUCCESS)
-//     {
-//         printf("Failed to subscribe, return code %d\n", rc);
-//         rc = EXIT_FAILURE;
-//     }
-//     else
-//     {
-//         int ch;
-//         do
-//         {
-//                 ch = getchar();
-//         } while (ch!='Q' && ch != 'q');
- 
-//         if ((rc = MQTTClient_unsubscribe(client, TOPIC)) != MQTTCLIENT_SUCCESS)
-//         {
-//                 printf("Failed to unsubscribe, return code %d\n", rc);
-//                 rc = EXIT_FAILURE;
-//         }
-//     }
- 
-//     if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
-//     {
-//         printf("Failed to disconnect, return code %d\n", rc);
-//         rc = EXIT_FAILURE;
-//     }
-// destroy_exit:
-//     MQTTClient_destroy(&client);
-// exit:
-//     return rc;
-
-    char* json_str = (char*) malloc(50*sizeof(char));
-    strcpy(json_str, "{ \"msg\": \"hehe\"}");
-    cJSON *root = cJSON_Parse(json_str);
-    if (root != NULL) {
-        // printf("JSON parse error\n");
-        cJSON *msg = cJSON_GetObjectItem(root, "msg");
-        if (cJSON_IsString(msg) && msg->valuestring != NULL) {
-            printf("msg: %s\n", msg->valuestring);
-        }
-
-        cJSON_Delete(root);
+    if ((rc = MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to create client, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+        goto exit;
     }
-    free(json_str);
+ 
+    if ((rc = MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to set callbacks, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+        goto destroy_exit;
+    }
+ 
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+        goto destroy_exit;
+    }
+ 
+    printf("Subscribing to topic %s\nfor client %s using QoS%d\n\n"
+           "Press Q<Enter> to quit\n\n", TOPIC, CLIENTID, QOS);
+    if ((rc = MQTTClient_subscribe(client, TOPIC, QOS)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to subscribe, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+    }
+    else
+    {
+        int ch;
+        do
+        {
+                ch = getchar();
+        } while (ch!='Q' && ch != 'q');
+ 
+        if ((rc = MQTTClient_unsubscribe(client, TOPIC)) != MQTTCLIENT_SUCCESS)
+        {
+                printf("Failed to unsubscribe, return code %d\n", rc);
+                rc = EXIT_FAILURE;
+        }
+    }
+ 
+    if ((rc = MQTTClient_disconnect(client, 10000)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to disconnect, return code %d\n", rc);
+        rc = EXIT_FAILURE;
+    }
+destroy_exit:
+    MQTTClient_destroy(&client);
+exit:
+    return rc;
+
+    // char* json_str = (char*) malloc(50*sizeof(char));
+    // strcpy(json_str, "{ \"msg\": \"hehe\"}");
+    // cJSON *root = cJSON_Parse(json_str);
+    // if (root != NULL) {
+    //     // printf("JSON parse error\n");
+    //     cJSON *msg = cJSON_GetObjectItem(root, "msg");
+    //     if (cJSON_IsString(msg) && msg->valuestring != NULL) {
+    //         printf("msg: %s\n", msg->valuestring);
+    //     }
+
+    //     cJSON_Delete(root);
+    // }
+    // free(json_str);
 }
---
+++ script.sh
@@ -0,0 +1,4 @@
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+./example/example
+
+sudo ldconfig
